f81cd2798454b56d5a56a33eb9859d81ecbf06b0
Giuss2 <120112582+Giuss2@users.noreply.github.com>
2023-03-04 11:56:19 +0100

    Update DAOEquipmentPostgres.java

--- src/main/java/DAOPostgresImplementation/DAOEquipmentPostgres.java
+++ src/main/java/DAOPostgresImplementation/DAOEquipmentPostgres.java
@@ -12,7 +12,7 @@ public class DAOEquipmentPostgres implements DAOEquipment {
 
     /**
      * Costruttore della classe. Ottiene il collegamento al database.
-     * @throws SQLException
+     * @throws SQLException Gestione delle eccezioni SQL
      */
     public DAOEquipmentPostgres() throws SQLException{
         connection = PostgresDBConnection.getInstance().getConnection();
@@ -20,14 +20,14 @@ public class DAOEquipmentPostgres implements DAOEquipment {
 
     /**
      * Metodo che inserisce una nuova tupla nella tabella Equipment
-     * @param id ID dell'equipment. Chiave primaria.
-     * @param name Nome dell'attrezzatura.
-     * @param description Descrizione dell'attrezzatura.
-     * @param price Prezzo d'acquisto dell'attrezzatura.
-     * @param purchaseDate Data d'acquisto dell'attrezzatura.
-     * @param dealer Fornitore dell'attrezzatura.
-     * @param laboratoryName Nome del laboratorio nel quale e' attualmente utilizzata l'attrezzatura.
-     * @param projectCup CUP del progetto che ha acquistato l'attrezzatura.
+     * @param id                ID dell'equipment. Chiave primaria.
+     * @param name              Nome dell'attrezzatura.
+     * @param description       Descrizione dell'attrezzatura.
+     * @param price             Prezzo d'acquisto dell'attrezzatura.
+     * @param purchaseDate      Data d'acquisto dell'attrezzatura.
+     * @param dealer            Fornitore dell'attrezzatura.
+     * @param laboratoryName    Nome del laboratorio nel quale è attualmente utilizzata l'attrezzatura.
+     * @param projectCup        CUP del progetto che ha acquistato l'attrezzatura.
      */
     public void addEquipmentDB(int id, String name, String description, float price, Date purchaseDate, String dealer, String laboratoryName,
                                String projectCup){
@@ -55,15 +55,15 @@ public class DAOEquipmentPostgres implements DAOEquipment {
 
     /**
      * Metodo che aggiorna la tupla Equipment con l'id passato in input.
-     * @param old_id Vecchio ID dell'equipment. Chiave primaria.
-     * @param new_id Nuovo ID dell'equipment. Chiave primaria.
-     * @param name Nome dell'attrezzatura.
-     * @param description Descrizione dell'attrezzatura.
-     * @param price Prezzo d'acquisto dell'attrezzatura.
-     * @param purchaseDate Data d'acquisto dell'attrezzatura.
-     * @param dealer Fornitore dell'attrezzatura.
-     * @param laboratoryName Nome del laboratorio nel quale è attualmente utilizzata l'attrezzatura.
-     * @param projectCup CUP del progetto che ha acquistato l'attrezzatura.
+     * @param old_id                Vecchio ID dell'equipment. Chiave primaria.
+     * @param new_id Nuovo          ID dell'equipment. Chiave primaria.
+     * @param name                  Nome dell'attrezzatura.
+     * @param description           Descrizione dell'attrezzatura.
+     * @param price                 Prezzo d'acquisto dell'attrezzatura.
+     * @param purchaseDate          Data d'acquisto dell'attrezzatura.
+     * @param dealer                Fornitore dell'attrezzatura.
+     * @param laboratoryName        Nome del laboratorio nel quale è attualmente utilizzata l'attrezzatura.
+     * @param projectCup            CUP del progetto che ha acquistato l'attrezzatura.
      */
     public void updateEquipmentDB(int old_id, int new_id, String name, String description, float price, Date purchaseDate, String dealer, String laboratoryName,
                                   String projectCup){
@@ -109,14 +109,14 @@ public class DAOEquipmentPostgres implements DAOEquipment {
 
     /**
      * Metodo che inserisce nelle liste passate per riferimento i dati di ogni singola tupla della tabella Equipment
-     * @param idList Lista degli id.
-     * @param nameList Lista dei nomi.
-     * @param descriptionList Lista delle descrizioni.
-     * @param priceList Lista dei prezzi.
-     * @param purchaseDateList Lista delle date d'acquisto.
-     * @param dealerList Lista dei fornitori.
-     * @param laboratoryNameList Lista dei laboratori.
-     * @param projectCupList Lista dei cup dei progetti.
+     * @param idList                Lista degli id.
+     * @param nameList              Lista dei nomi.
+     * @param descriptionList       Lista delle descrizioni.
+     * @param priceList             Lista dei prezzi.
+     * @param purchaseDateList      Lista delle date d'acquisto.
+     * @param dealerList            Lista dei fornitori.
+     * @param laboratoryNameList    Lista dei laboratori.
+     * @param projectCupList        Lista dei cup dei progetti.
      */
     public void loadEquipmentDB(ArrayList<Integer> idList, ArrayList<String> nameList, ArrayList<String> descriptionList, ArrayList<Float> priceList,
                               ArrayList<Date> purchaseDateList, ArrayList<String> dealerList, ArrayList<String> laboratoryNameList,
