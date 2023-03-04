b46448df1d11d6688ed6befc5c79f5e2e5f84a6f
Giuss2 <120112582+Giuss2@users.noreply.github.com>
2023-03-04 12:02:40 +0100

    Update addEmployeeController.java

--- src/main/java/GUI/addEmployeeController.java
+++ src/main/java/GUI/addEmployeeController.java
@@ -39,7 +39,7 @@ public class addEmployeeController implements Initializable {
 
     /**
      * Metodo che viene chiamato quando l'utente conferma l'inserimento.
-     * Esegue i controlli sulla validita' dell'input e passa i dati inseriti dall'utente al controller che crea l'oggetto impiegato.
+     * Esegue i controlli sulla validità dell'input e passa i dati inseriti dall'utente al controller che crea l'oggetto impiegato.
      */
     @FXML void hireEmployee() throws IOException{
         ArrayList<String> errors = new ArrayList<String>();
@@ -90,8 +90,8 @@ public class addEmployeeController implements Initializable {
 
     /**
      * Metodo che apre una finestra elencando gli errori passati in input.
-     * @param errors
-     * @throws IOException
+     * @param errors Stringhe di errori rilevati
+     * @throws IOException Gestione delle eccezioni di I/O
      */
     private void showErrorWindow (ArrayList<String> errors) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../GUI/ErrorWindow.fxml"));
