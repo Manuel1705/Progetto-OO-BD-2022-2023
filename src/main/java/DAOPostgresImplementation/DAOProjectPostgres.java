7cd7b1593d410084671913e0f7558e4fea9d0014
Giuss2 <120112582+Giuss2@users.noreply.github.com>
2023-03-04 11:59:12 +0100

    Update DAOProjectPostgres.java

--- src/main/java/DAOPostgresImplementation/DAOProjectPostgres.java
+++ src/main/java/DAOPostgresImplementation/DAOProjectPostgres.java
@@ -12,7 +12,7 @@ public class DAOProjectPostgres implements DAOProject {
 
     /**
      * Costruttore della classe. Ottiene il collegamento al database.
-     * @throws SQLException
+     * @throws SQLException Gestione delle eccezioni SQL
      */
     public DAOProjectPostgres() throws SQLException{
         connection = PostgresDBConnection.getInstance().getConnection();
@@ -21,14 +21,14 @@ public class DAOProjectPostgres implements DAOProject {
 
     /**
      * Metodo che inserisce una nuova tupla nella tabella Project
-     * @param cup Codice Unico Progetto del progetto. Chiave primaria.
-     * @param name Nome del progetto.
-     * @param budget Budget del progetto.
-     * @param remainingFunds Fondi rimanenti del progetto.
-     * @param startDate Data in cui e' iniziato il progetto.
-     * @param endDate Data per cui e' prevista la fine del progetto.
-     * @param sResp Responsabile scientifico del progetto.
-     * @param sRef Referente scientifico del progetto.
+     * @param cup               Codice Unico Progetto del progetto. Chiave primaria.
+     * @param name              Nome del progetto.
+     * @param budget            Budget del progetto.
+     * @param remainingFunds    Fondi rimanenti del progetto.
+     * @param startDate         Data in cui è iniziato il progetto.
+     * @param endDate           Data per cui è prevista la fine del progetto.
+     * @param sResp             Responsabile scientifico del progetto.
+     * @param sRef              Referente scientifico del progetto.
      */
     public void addProjectDB(String cup, String name, float budget, float remainingFunds, Date startDate, Date endDate,
                              String sResp, String sRef){
@@ -73,15 +73,15 @@ public class DAOProjectPostgres implements DAOProject {
 
     /**
      * Metodo che aggiorna la tupla Project con chiave primaria uguale a oldCup
-     * @param oldCup Chiave primaria originale.
-     * @param newCup Nuovo CUP del progetto.
-     * @param name Nome del progetto.
-     * @param budget Budget del progetto.
-     * @param remainingFunds Fondi rimanenti del progetto.
-     * @param startDate Data in cui e' iniziato il progetto.
-     * @param endDate Data per cui e' prevista la fine del progetto.
-     * @param sResp Responsabile scientifico del progetto.
-     * @param sRef Referente scientifico del progetto.
+     * @param oldCup            Chiave primaria originale.
+     * @param newCup            Nuovo CUP del progetto.
+     * @param name              Nome del progetto.
+     * @param budget            Budget del progetto.
+     * @param remainingFunds    Fondi rimanenti del progetto.
+     * @param startDate         Data in cui è iniziato il progetto.
+     * @param endDate           Data per cui è prevista la fine del progetto.
+     * @param sResp             Responsabile scientifico del progetto.
+     * @param sRef              Referente scientifico del progetto.
      */
     public void updateProjectDB(String oldCup, String newCup, String name, float budget, float remainingFunds, Date startDate, Date endDate,
                                 String sResp, String sRef){
@@ -110,8 +110,8 @@ public class DAOProjectPostgres implements DAOProject {
 
     /**
      * Aggiorna i fondi rimanenti del progetto.
-     * @param cup
-     * @param remainingFunds
+     * @param cup Cup del progetto
+     * @param remainingFunds Fondi rimanenti da modificare
      */
     public void updateProjectDBRemainingFunds(String cup, float remainingFunds){
         String query = "UPDATE azienda.project " +
@@ -134,13 +134,13 @@ public class DAOProjectPostgres implements DAOProject {
     /**
      *
      * @param cupList Lista dei CUP delle tuple.
-     * @param nameList Lista dei nomi delle tuple.
-     * @param budgetList Lista dei budget delle tuple.
-     * @param remainingFundsList Lista dei fondi rimanenti delle tuple.
-     * @param startDateList Lista delle date di inizio delle tuple.
-     * @param endDateList Lista delle date di fine delle tuple.
-     * @param sRespList Lista dei responsabili scientifici delle tuple.
-     * @param sRefList Lista dei referenti scientifici delle tuple.
+     * @param nameList              Lista dei nomi delle tuple.
+     * @param budgetList            Lista dei budget delle tuple.
+     * @param remainingFundsList    Lista dei fondi rimanenti delle tuple.
+     * @param startDateList         Lista delle date di inizio delle tuple.
+     * @param endDateList           Lista delle date di fine delle tuple.
+     * @param sRespList             Lista dei responsabili scientifici delle tuple.
+     * @param sRefList              Lista dei referenti scientifici delle tuple.
      */
     public void loadProjectDB(ArrayList<String> cupList, ArrayList<String> nameList, ArrayList<Float> budgetList, ArrayList<Float> remainingFundsList,
                               ArrayList<Date> startDateList, ArrayList<Date> endDateList, ArrayList<String> sRespList, ArrayList<String> sRefList){
