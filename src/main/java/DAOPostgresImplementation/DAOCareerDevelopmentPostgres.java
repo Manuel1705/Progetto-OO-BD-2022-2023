dfbc37d00e5b1c0f8749635f7d04501dd6a8ee9a
Giuss2 <120112582+Giuss2@users.noreply.github.com>
2023-03-04 11:44:02 +0100

    Update DAOCareerDevelopmentPostgres.java

--- src/main/java/DAOPostgresImplementation/DAOCareerDevelopmentPostgres.java
+++ src/main/java/DAOPostgresImplementation/DAOCareerDevelopmentPostgres.java
@@ -12,29 +12,19 @@ public class DAOCareerDevelopmentPostgres implements DAOCareerDevelopment {
 
     /**
      * Costruttore della classe. Ottiene il collegamento al database.
-     * @throws SQLException
+     * @throws SQLException Gestione delle eccezioni SQL
      */
     public DAOCareerDevelopmentPostgres() throws SQLException{
         connection = PostgresDBConnection.getInstance().getConnection();
     }
 
-
-    /**
-     * Metodo che inserisce una nuova tupla nella tabella CareerDevelopment
-     * @param oldRole Vecchio ruolo dell'impiegato
-     * @param newRole Nuovo ruolo dell'impiegato
-     * @param roleChangeDate Data scatto di carriera
-     * @param salaryChange Differenza tra salario nuovo e salario vecchio
-     * @param ssn SSN dell'impiegato
-     */
-
     /**
      * Metodo che carica tutte le tuple della tabella Career Development
-     * @param oldRoleList Lista dei vecchi ruoli.
-     * @param newRoleList Lista dei nuovi ruoli.
-     * @param roleChangeDateList Lista delle date degli scatti di carriera.
-     * @param salaryChangeList Lista dei cambi di salario.
-     * @param ssnList Lista degli ssn.
+     * @param oldRoleList           Lista dei vecchi ruoli.
+     * @param newRoleList           Lista dei nuovi ruoli.
+     * @param roleChangeDateList    Lista delle date degli scatti di carriera.
+     * @param salaryChangeList      Lista dei cambi di salario.
+     * @param ssnList               Lista degli ssn.
      */
     public void loadCareerDevelopment(ArrayList<String> oldRoleList, ArrayList<String> newRoleList, ArrayList<Date> roleChangeDateList,
                                       ArrayList<Float> salaryChangeList, ArrayList<String> ssnList){
